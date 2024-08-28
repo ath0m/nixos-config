d041aa300fc29d65d7d3dd627e87b2d02273811e
Tomasz Nanowski <tomasz.nanowski@gmail.com>
2024-08-28 22:34:08 +0100

    Init nixos config

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "NixOS systems and tools";
+
+  inputs = {
+    # Pin our primary nixpkgs repository. This is the main nixpkgs repository
+    # we'll use for our configurations. Be very careful changing this because
+    # it'll impact your entire system.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+
+    # We use the unstable nixpkgs repo for some packages.
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # I think technically you're not supposed to override the nixpkgs
+    # used by neovim but recently I had failures if I didn't pin to my
+    # own. We can always try to remove that anytime.
+    neovim-nightly-overlay = {
+      url = "github:nix-community/neovim-nightly-overlay";
+
+      # Only need unstable until the lpeg fix hits mainline, probably
+      # not very long... can safely switch back for 23.11.
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+
+    # Non-flakes
+    nvim-cinnamon.url = "github:declancm/cinnamon.nvim";
+    nvim-cinnamon.flake = false;
+    nvim-conform.url = "github:stevearc/conform.nvim/v5.2.1";
+    nvim-conform.flake = false;
+    nvim-treesitter.url = "github:nvim-treesitter/nvim-treesitter/v0.9.1";
+    nvim-treesitter.flake = false;
+    vim-copilot.url = "github:github/copilot.vim/v1.32.0";
+    vim-copilot.flake = false;
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: let
+    # Overlays is the list of overlays we want to apply from flake inputs.
+    overlays = [ ];
+
+    mkSystem = import ./lib/mksystem.nix {
+      inherit overlays nixpkgs inputs;
+    };
+  in {
+    nixosConfigurations.vm-aarch64 = mkSystem "vm-aarch64" {
+      system = "aarch64-linux";
+      user   = "tomek";
+    };
+  };
+}
---
+++ users/tomek/config.fish
@@ -0,0 +1,130 @@
+#-------------------------------------------------------------------------------
+# SSH Agent
+#-------------------------------------------------------------------------------
+function __ssh_agent_is_started -d "check if ssh agent is already started"
+	if begin; test -f $SSH_ENV; and test -z "$SSH_AGENT_PID"; end
+		source $SSH_ENV > /dev/null
+	end
+
+	if test -z "$SSH_AGENT_PID"
+		return 1
+	end
+
+	ssh-add -l > /dev/null 2>&1
+	if test $status -eq 2
+		return 1
+	end
+end
+
+function __ssh_agent_start -d "start a new ssh agent"
+  ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
+  chmod 600 $SSH_ENV
+  source $SSH_ENV > /dev/null
+  ssh-add
+end
+
+if not test -d $HOME/.ssh
+    mkdir -p $HOME/.ssh
+    chmod 0700 $HOME/.ssh
+end
+
+if test -d $HOME/.gnupg
+    chmod 0700 $HOME/.gnupg
+end
+
+if test -z "$SSH_ENV"
+    set -xg SSH_ENV $HOME/.ssh/environment
+end
+
+if not __ssh_agent_is_started
+    __ssh_agent_start
+end
+
+#-------------------------------------------------------------------------------
+# Ghostty Shell Integration
+#-------------------------------------------------------------------------------
+# Ghostty supports auto-injection but Nix-darwin hard overwrites XDG_DATA_DIRS
+# which make it so that we can't use the auto-injection. We have to source
+# manually.
+if set -q GHOSTTY_RESOURCES_DIR
+    source "$GHOSTTY_RESOURCES_DIR/shell-integration/fish/vendor_conf.d/ghostty-shell-integration.fish"
+end
+
+#-------------------------------------------------------------------------------
+# Programs
+#-------------------------------------------------------------------------------
+# Vim: We should move this somewhere else but it works for now
+mkdir -p $HOME/.vim/{backup,swap,undo}
+
+# Add ~/.local/bin
+set -q PATH; or set PATH ''; set -gx PATH  "$HOME/.local/bin" $PATH;
+
+#-------------------------------------------------------------------------------
+# Prompt
+#-------------------------------------------------------------------------------
+# Do not show any greeting
+set --universal --erase fish_greeting
+function fish_greeting; end
+funcsave fish_greeting
+
+# My color scheme
+set -g fish_color_normal cdd6f4
+set -g fish_color_command 89b4fa
+set -g fish_color_param f2cdcd
+set -g fish_color_keyword f38ba8
+set -g fish_color_quote a6e3a1
+set -g fish_color_redirection f5c2e7
+set -g fish_color_end fab387
+set -g fish_color_comment 7f849c
+set -g fish_color_error f38ba8
+set -g fish_color_gray 6c7086
+set -g fish_color_selection --background=313244
+set -g fish_color_search_match --background=313244
+set -g fish_color_option a6e3a1
+set -g fish_color_operator f5c2e7
+set -g fish_color_escape eba0ac
+set -g fish_color_autosuggestion 6c7086
+set -g fish_color_cancel f38ba8
+set -g fish_color_cwd f9e2af
+set -g fish_color_user 94e2d5
+set -g fish_color_host 89b4fa
+set -g fish_color_host_remote a6e3a1
+set -g fish_color_status f38ba8
+set -g fish_pager_color_progress 6c7086
+set -g fish_pager_color_prefix f5c2e7
+set -g fish_pager_color_completion cdd6f4
+set -g fish_pager_color_description 6c7086
+
+# Override the nix prompt for the theme so that we show a more concise prompt
+function __bobthefish_prompt_nix -S -d 'Display current nix environment'
+    [ "$theme_display_nix" = 'no' -o -z "$IN_NIX_SHELL" ]
+    and return
+
+    __bobthefish_start_segment $color_nix
+    echo -ns N ' '
+
+    set_color normal
+end
+
+#-------------------------------------------------------------------------------
+# Vars
+#-------------------------------------------------------------------------------
+# Modify our path to include our Go binaries
+contains $HOME/code/go/bin $fish_user_paths; or set -Ua fish_user_paths $HOME/code/go/bin
+contains $HOME/bin $fish_user_paths; or set -Ua fish_user_paths $HOME/bin
+
+# Exported variables
+if isatty
+    set -x GPG_TTY (tty)
+end
+
+# Editor
+set -gx EDITOR nvim
+
+#-------------------------------------------------------------------------------
+# Functions
+#-------------------------------------------------------------------------------
+# Shortcut to setup a nix-shell with fish. This lets you do something like
+# `fnix -p go` to get an environment with Go but use the fish shell along
+# with it.
+alias fnix "nix-shell --run fish"
