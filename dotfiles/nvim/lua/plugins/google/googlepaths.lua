070fe9309ec8f1db6766e55048d8b8dd4951c415
Tomasz Nanowski <tomasz.nanowski@gmail.com>
2024-10-11 00:53:06 +0100

    Updated nvim

--- dotfiles/nvim/lua/plugins/google/googlepaths.lua
+++
@@ -1,8 +0,0 @@
-return {
-  {
-    -- Support google paths such as `//google3/*`
-    url = "sso://user/fentanes/googlepaths.nvim",
-    event = { "VeryLazy", "BufReadCmd //*" },
-    opts = {},
-  },
-}
--- dotfiles/nvim/lua/plugins/obsidian.lua
+++ dotfiles/nvim/lua/plugins/obsidian.lua
@@ -1,5 +1,5 @@
 return {
-    {
+  {
     "epwalsh/obsidian.nvim",
     dependencies = {
       "hrsh7th/nvim-cmp",
@@ -10,12 +10,24 @@ return {
     },
     ft = { "markdown" },
     keys = {
-      { "<leader>nb", "<cmd>ObsidianBacklinks<cr>", desc = "obsidian backlinks" },
+      {
+        "<leader>nb",
+        "<cmd>ObsidianBacklinks<cr>",
+        desc = "obsidian backlinks",
+      },
       { "<leader>nf", "<cmd>ObsidianQuickSwitch<cr>", desc = "obsidian find" },
       { "<leader>ni", "<cmd>ObsidianPasteImg<cr>", desc = "obsidian img" },
-      { "<leader>njm", "<cmd>ObsidianTomorrow<cr>", desc = "obsidian tomorrow" },
+      {
+        "<leader>njm",
+        "<cmd>ObsidianTomorrow<cr>",
+        desc = "obsidian tomorrow",
+      },
       { "<leader>njt", "<cmd>ObsidianToday<cr>", desc = "obsidian today" },
-      { "<leader>njy", "<cmd>ObsidianYesterday<cr>", desc = "obsidian yesterday" },
+      {
+        "<leader>njy",
+        "<cmd>ObsidianYesterday<cr>",
+        desc = "obsidian yesterday",
+      },
       { "<leader>njd", "<cmd>ObsidianDailies<cr>", desc = "obsidian dailies" },
       { "<leader>nll", "<cmd>ObsidianLink<cr>", desc = "obsidian link" },
       { "<leader>nln", "<cmd>ObsidianLinkNew<cr>", desc = "obsidian link new" },
@@ -24,15 +36,27 @@ return {
       { "<leader>nr", "<cmd>ObsidianRename<cr>", desc = "obsidian rename" },
       { "<leader>ns", "<cmd>ObsidianSearch<cr>", desc = "obsidian search" },
       { "<leader>nt", "<cmd>ObsidianTags<cr>", desc = "obsidian tags" },
-      { "<leader>nwm", "<cmd>ObsidianWorkspace main<cr>", desc = "select workspace main" },
-      { "<localleader>mx", function() require("obsidian.util").toggle_checkbox() end, desc = "mark complete" },
+      {
+        "<leader>nwm",
+        "<cmd>ObsidianWorkspace main<cr>",
+        desc = "select workspace main",
+      },
+      {
+        "<localleader>mx",
+        function()
+          require("obsidian.util").toggle_checkbox()
+        end,
+        desc = "mark complete",
+      },
     },
     opts = {
       daily_notes = {
         alias_format = "%Y-%m-%d",
         folder = "journal",
       },
-      follow_url_func = function(url) vim.fn.jobstart({ "open", url }) end,
+      follow_url_func = function(url)
+        vim.fn.jobstart({ "open", url })
+      end,
       note_id_func = function(title)
         -- Given "Note Name" will make ID `<timestamp>-note-name`.
         -- Filename is the same with `.md` suffix.
@@ -51,9 +75,9 @@ return {
       ui = { enable = false },
       workspaces = {
         {
-          name = 'main',
-          path = '~/G-Brain'
-        }
+          name = "main",
+          path = "~/rain",
+        },
       },
     },
   },
@@ -76,8 +100,18 @@ return {
         go_parent_heading = "[p",
       },
       on_attach = function(bufnr)
-        vim.keymap.set({ "n", "i" }, "<m-o>", "<cmd>MDListItemBelow<cr>", { buffer = bufnr, desc = "item below" })
-        vim.keymap.set({ "n", "i" }, "<m-O>", "<cmd>MDListItemAbove<cr>", { buffer = bufnr, desc = "item above" })
+        vim.keymap.set(
+          { "n", "i" },
+          "<m-o>",
+          "<cmd>MDListItemBelow<cr>",
+          { buffer = bufnr, desc = "item below" }
+        )
+        vim.keymap.set(
+          { "n", "i" },
+          "<m-O>",
+          "<cmd>MDListItemAbove<cr>",
+          { buffer = bufnr, desc = "item above" }
+        )
       end,
     },
   },
